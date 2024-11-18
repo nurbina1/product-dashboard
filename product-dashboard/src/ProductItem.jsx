7729e857b808e2d181181feb4f35b501ff82c7bc
Abylkhair03 <nurbina@usf.edu>
2024-11-17 21:49:12 -0500

    Create ProductItem component to display individual product details

---
+++ product-dashboard/src/ProductItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ProductItem = ({ product }) => {
+  return (
+    <div>
+      <h3>{product.name}</h3>
+      <p>Price: ${product.price}</p>
+      <p>{product.description}</p>
+    </div>
+  );
+};
+
+export default ProductItem;
